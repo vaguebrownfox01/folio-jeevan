775a2744853109ac2c12d2b85b398d8ce1139ace
vaguebrownfox <foxjournal.nsfw@gmail.com>
2023-01-19 09:56:25 +0530

    no hide

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -14,12 +14,12 @@ const LayoutWrapper = ({ children }) => {
         <header className="flex items-center justify-between py-10">
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
+              <div className="flex items-center justify-center">
+                <div className="mr-2 sm:mr-3">
                   <Logo />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  <div className="text-1.5xl sm: sm:text-2.5xl h-6 font-semibold leading-8  sm:block md:text-2xl ">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
