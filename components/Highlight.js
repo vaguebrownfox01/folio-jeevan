e2dbabebb3b254ed9604894e9fb383b08bebb64f
vaguebrownfox <foxjournal.nsfw@gmail.com>
2023-05-12 07:55:55 +0530

    additional articles

--- components/Highlight.js
+++ components/Highlight.js
@@ -6,14 +6,14 @@ export function Hil({ children, color }) {
 }
 export function Ul({ children, color }) {
   return (
-    <span className="rounded-[2px] p-1 text-white underline decoration-primary-500 decoration-2 underline-offset-4">
+    <span className="rounded-[2px] p-1 underline decoration-primary-500 decoration-2 underline-offset-4 dark:text-white">
       {children}
     </span>
   )
 }
 
 export function Ref({ children, color }) {
-  return <span className="rounded-[2px] p-1 text-primary-500">{children}</span>
+  return <span className="rounded-[2px] p-1 font-semibold text-primary-600">{children}</span>
 }
 
 export function Link({ href }) {
@@ -26,17 +26,26 @@ export function Link({ href }) {
 
 export function Next({ next_page, prev_page }) {
   return (
-    <div className="flex justify-around px-8 py-2">
-      {prev_page && (
-        <a href={prev_page} className="rounded-[2px] bg-gray-800 px-4 py-1 text-primary-500">
-          {`PREV`}
-        </a>
-      )}
-      {next_page && (
-        <a href={next_page} className="rounded-[2px] bg-gray-800 px-4 py-1 text-primary-500">
-          {`NEXT`}
-        </a>
-      )}
-    </div>
+    <>
+      <br />
+      <div className="flex justify-around px-8 py-2">
+        {prev_page && (
+          <a
+            href={prev_page.href}
+            className="rounded-[2px] bg-gray-100 px-4 py-1 text-primary-500 dark:bg-gray-800"
+          >
+            {`Previous: ${prev_page.label}`}
+          </a>
+        )}
+        {next_page && (
+          <a
+            href={next_page.href}
+            className="rounded-[2px] bg-gray-100 px-4 py-1 text-primary-500 dark:bg-gray-800"
+          >
+            {`Next: ${next_page.label}`}
+          </a>
+        )}
+      </div>
+    </>
   )
 }
