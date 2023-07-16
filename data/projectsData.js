0221964e5756a3e91057168d6f7581c6149d3174
Jeevan Kylash <jeevankylash@outlook.com>
2023-07-16 23:26:49 +0000

    path fix

--- data/projectsData.js
+++ data/projectsData.js
@@ -3,7 +3,7 @@ const projectsData = [
     title: 'Pnoi-phone',
     description: `An acoustic-based method for detection and monitoring of respiratory diseases using simultaneously recorded vocal breath sounds and pulmonary(chest) auscultation.`,
     imgSrc: '/static/images/pnoi/pnoi-illustration.png',
-    href: 'all/pnoi-phone/1_introduction',
+    href: 'all/project/pnoi-phone/1_introduction',
   },
   // {
   //   title: 'The Time Machine',
